e3076ceb7c884080f7bb6d5a4ae240ab90d8a3f7
TinasheCalvin <makaratinashe22@gmail.com>
2022-05-10 22:42:44 +0200

    adde the important tasks screen

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
 import styled from 'styled-components'
-import Main from './components/Main';
-import Sidebar from './components/Sidebar';
+import Main from './components/Main'
+import Sidebar from './components/Sidebar'
+import Important from './pages/Important'
 
 function App() {
   return (
@@ -11,7 +12,7 @@ function App() {
         <Routes>
           <Route path="/">
             <Route index element={<Main background="/images/TV Tower.jpg" />} />
-            <Route path="important" element={<Main background="/images/Beach.jpg" />} />
+            <Route path="important" element={<Important background="/images/Beach.jpg" />} />
             <Route path="planned" element={<Main background="/images/Sunset.jpg" />} />
             <Route path="assigned" element={<Main background="/images/Desert.jpg" />} />
             <Route path="tasks" element={<Main background="/images/Lighthouse.jpg" />} />
