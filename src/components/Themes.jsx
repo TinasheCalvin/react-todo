1aae806416a879805e1028878539ca10bcc338dc
TinasheCalvin <makaratinashe22@gmail.com>
2022-05-15 22:03:36 +0200

    added images on the themes component

--- src/components/Themes.jsx
+++ src/components/Themes.jsx
@@ -3,33 +3,109 @@ import styled from 'styled-components'
 
 function Themes({visible}) {
   return (
-    <ThemesContainer isVisible={visible}>
-        <span>Hey Calvin</span>
-    </ThemesContainer>
+    <Container isVisible={visible}>
+        <span>Themes</span>
+        <span className="desc">Choose desired theme from the ones given below</span>
+        <ThemesWrapper>
+            <Theme>
+                <img src='/images/BirdNest.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Fern.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Safari.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Beach.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Desert.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Field.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Gradient.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Lighthouse.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Sea.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Sunrise.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/Sunset.jpg' alt='' />
+            </Theme>
+            <Theme>
+                <img src='/images/TV Tower.jpg' alt='' />
+            </Theme>
+        </ThemesWrapper>
+    </Container>
   )
 }
 
 export default Themes
 
-const ThemesContainer = styled.div`
+const Container = styled.div`
     position: absolute;
     top: 15%;
     right: 0;
     width: 250px;
     height: 250px;
+    padding: 10px;
     background-color: #282828;
     border-radius: 5px 0 0 5px;
     z-index: 0;
     border: none;
     display: none;
 
+    span {
+        font-size: 14px;
+
+        &.desc {
+            display: block;
+            font-weight: 300;
+            font-size: 10px;
+        }
+    }
+
     ${({isVisible}) => {
         if (isVisible) {
             return `
-                display: flex;
-                justify-content: center;
-                align-items: center;
+                display: block;
             `
         }
     }}
+`
+
+const ThemesWrapper = styled.div`
+    display: flex;
+    justify-content: space-around;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 10px;
+    margin-top: 10px;
+`
+
+const Theme = styled.div`
+    width: 45px;
+    height: 45px;
+    border: none;
+    cursor: pointer;
+    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+    &:hover {
+        transform: scale(1.03);
+    }
+
+    img {
+        width: 100%;
+        height: 100%;
+        border-radius: 4px;
+        object-fit: cover;
+    }
 `
